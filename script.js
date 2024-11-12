db11ff90260d060125e97fc1be4ba105f4b91e9a
fatima498 <nexonsolutions2@gmail.com>
2024-11-12 21:46:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Get references to the calculator screen and buttons
+let display = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let buttonArray = Array.from(buttons);
+
+let string = "";
+
+buttonArray.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+
+    if (event.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    }
+    
+    else if (event.target.innerHTML == "AC") {
+      string = "";
+      display.value = string;
+    } 
+    
+    else if (event.target.innerHTML == "=") {
+      string = eval(string);
+      display.value = string;
+    } 
+    
+    else {
+      string += event.target.innerHTML;
+      display.value = string;
+    }
+  });
+});
